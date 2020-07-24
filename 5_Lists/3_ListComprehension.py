c33f482767db45972a11fc994972823cd9b0fac6
Qaidjohar <qaidjawadwala@gmail.com>
2020-07-24 20:32:03 +0530

    List Comprehension

---
+++ 5_Lists/3_ListComprehension.py
@@ -0,0 +1,25 @@
+# name = 'qaidjohar'
+# print('Input is', name)
+# l = []
+# for n in name:
+#     n = n.upper()
+#     l.append(n)
+
+# print(l)
+
+
+# l = [n.upper() for n in name]
+# print('List Comprehension output', l)
+
+
+number = 10
+
+num_list = [num**5 for num in range(number)]
+print(num_list)
+
+
+# HomeWork ProblemSet
+# (0°C × 9/5) + 32 = 32°F
+
+# celsius = [5, 22, 43, 64, 54, 12, 69, 78, 92]
+# fahrenheit = [????]
